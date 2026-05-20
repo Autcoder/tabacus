5d211516c676d6533853c3a88c82a2a989fc719c
Autcoder <150786670+Autcoder@users.noreply.github.com>
2026-05-20 14:22:06 +0200

    Implement Shunting Yard Parser

---
+++ src/parser.nim
@@ -0,0 +1,74 @@
+import lexer
+
+proc precedence(k: Kind): int =
+  case k
+  of tkPlus, tkMinus:
+    1
+  of tkTimes, tkDiv, tkMod:
+    2
+  of tkUnaryMinus:
+    3
+  of tkPower:
+    4
+  else:
+    0
+
+proc isRightAssociative(k: Kind): bool =
+  k in {tkPower, tkUnaryMinus}
+
+proc isOperator(k: Kind): bool =
+  k in {
+    tkPlus,
+    tkMinus,
+    tkUnaryMinus,
+    tkTimes,
+    tkDiv,
+    tkPower,
+    tkMod
+  }
+
+proc shuntingYard*(tokens: seq[Token]): seq[Token] =
+  var output: seq[Token]
+  var stack: seq[Token]
+
+  for token in tokens:
+
+    case token.kind
+
+    of tkInt, tkFloat, tkConst:
+      output.add(token)
+
+    of tkFunc:
+      stack.add(token)
+
+    of tkComma:
+      while stack.len > 0 and stack[^1].kind != tkLPar:
+        output.add(stack.pop())
+
+    of tkLPar:
+      stack.add(token)
+
+    of tkRPar:
+      while stack.len > 0 and stack[^1].kind != tkLPar:
+        output.add(stack.pop())
+      if stack.len > 0:
+        discard stack.pop()
+      if stack.len > 0 and stack[^1].kind == tkFunc:
+        output.add(stack.pop())
+
+    else:
+      if isOperator(token.kind):
+
+        while stack.len > 0 and
+          isOperator(stack[^1].kind) and
+          (precedence(stack[^1].kind) > precedence(token.kind) or
+          (precedence(stack[^1].kind) == precedence(token.kind) and not
+          isRightAssociative(token.kind))):
+            output.add(stack.pop())
+
+        stack.add(token)
+
+  while stack.len > 0:
+    output.add(stack.pop())
+
+  return output
